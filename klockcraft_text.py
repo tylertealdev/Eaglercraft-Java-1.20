263c04777f4e9779c5a969ffe5d3de13cd0a7052
tylertealdev <tyleriteal@proton.me>
2024-10-12 16:52:09 -0400

    Update klockcraft_text.py

--- klockcraft_text.py
+++ klockcraft_text.py
@@ -1,195 +1,160 @@
-import turtle
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
 
-class KlockcraftText:
-    def __init__(self):
-        turtle.tracer(0)
-        self.pen = turtle.Turtle()
-        self.pen.hideturtle()
-        self.pen.speed(0)
-    def draw_text(self, message, color, size, x, y):
-        on_letter = -1
-        for letter in message:
-            on_letter += 1
-            x += pow(size, size)*pow(size, size)
-            if letter == 'a':
-                self.draw_square(color, size, x+size, y)
-                self.draw_square(color, size, x+size*2, y)
-                self.draw_square(color, size, x+size*3, y)
-                self.draw_square(color, size, x+size*4, y-size)
-                self.draw_square(color, size, x+size*4, y-(size*2))
-                self.draw_square(color, size, x+size*3, y-(size*2))
-                self.draw_square(color, size, x+size*2, y-(size*2))
-                self.draw_square(color, size, x+size*1, y-(size*2))
-                self.draw_square(color, size, x+size*4, y-(size*3))
-                self.draw_square(color, size, x+size*4, y-(size*4))
-                self.draw_square(color, size, x+size*3, y-(size*4))
-                self.draw_square(color, size, x+size*2, y-(size*4))
-                self.draw_square(color, size, x+size*1, y-(size*4))
-                self.draw_square(color, size, x, y-(size*3))
-            elif letter == 'b':
-                self.draw_square(color, size, x-(size*1), y+(size*3))
-                self.draw_square(color, size, x-(size*1), y+(size*2))
-                self.draw_square(color, size, x-(size*1), y+(size*1))
-                self.draw_square(color, size, x-(size*1), y)
-                self.draw_square(color, size, x-(size*1), y-(size*1))
-                self.draw_square(color, size, x-(size*1), y-(size*2))
-                self.draw_square(color, size, x-(size*1), y-(size*3))
-                self.draw_square(color, size, x-(size*1), y-(size*4))
-                self.draw_square(color, size, x+size-(size*1), y-(size*1))
-                self.draw_square(color, size, x+(size*2)-(size*1), y)
-                self.draw_square(color, size, x+(size*3)-(size*1), y)
-                self.draw_square(color, size, x+(size*4)-(size*1), y-(size*1))
-                self.draw_square(color, size, x+(size*4)-(size*1), y-(size*2))
-                self.draw_square(color, size, x+(size*4)-(size*1), y-(size*3))
-                self.draw_square(color, size, x+(size*3)-(size*1), y-(size*4))
-                self.draw_square(color, size, x+(size*2)-(size*1), y-(size*4))
-                self.draw_square(color, size, x+(size*1)-(size*1), y-(size*4))
-            elif letter == 'c':
-                self.draw_square(color, size, x-(size*4)-(size*1), y-(size*1))
-                self.draw_square(color, size, x-(size*4)-(size*1), y-(size*2))
-                self.draw_square(color, size, x-(size*4)-(size*1), y-(size*3))
-                self.draw_square(color, size, x-(size*3)-(size*1), y-(size*4))
-                self.draw_square(color, size, x-(size*2)-(size*1), y-(size*4))
-                self.draw_square(color, size, x-(size*1)-(size*1), y-(size*4))
-                self.draw_square(color, size, x-(size*1), y-(size*3))
-                self.draw_square(color, size, x-(size*2)-(size*2), y)
-                self.draw_square(color, size, x-(size*1)-(size*2), y)
-                self.draw_square(color, size, x-(size*2), y)
-                self.draw_square(color, size, x-(size*1), y-(size*1))
-            elif letter == 'd':
-                self.draw_square(color, size, x, y+(size*5))
-                self.draw_square(color, size, x, y+(size*4))
-                self.draw_square(color, size, x, y+(size*3))
-                self.draw_square(color, size, x, y+(size*2))
-                self.draw_square(color, size, x-(size*1), y)
-                self.draw_square(color, size, x-(size*2), y+(size*1))
-                self.draw_square(color, size, x-(size*3), y+(size*1))
-                self.draw_square(color, size, x-(size*4), y)
-                self.draw_square(color, size, x-(size*4), y-(size*1))
-                self.draw_square(color, size, x-(size*4), y-(size*2))
-                self.draw_square(color, size, x-(size*4), y-(size*3))
-                self.draw_square(color, size, x-(size*3), y-(size*4))
-                self.draw_square(color, size, x-(size*2), y-(size*4))
-                self.draw_square(color, size, x-(size*1), y-(size*4))
-                self.draw_square(color, size, x, y+(size*1))
-                self.draw_square(color, size, x, y)
-                self.draw_square(color, size, x, y-(size*1))
-                self.draw_square(color, size, x, y-(size*2))
-                self.draw_square(color, size, x, y-(size*3))
-                self.draw_square(color, size, x, y-(size*4))
-            elif letter == 'e':
-                self.draw_square(color, size, x, y)
-                self.draw_square(color, size, x+(size*1), y)
-                self.draw_square(color, size, x+(size*2), y)
-                self.draw_square(color, size, x+(size*3), y)
-                self.draw_square(color, size, x+(size*3), y+(size*1))
-                self.draw_square(color, size, x+(size*2), y+(size*2))
-                self.draw_square(color, size, x+(size*1), y+(size*2))
-                self.draw_square(color, size, x, y+(size*2))
-                self.draw_square(color, size, x-(size*1), y+(size*1))
-                self.draw_square(color, size, x-(size*1), y)
-                self.draw_square(color, size, x-(size*1), y-(size*1))
-                self.draw_square(color, size, x, y-(size*2))
-                self.draw_square(color, size, x+(size*1), y-(size*2))
-                self.draw_square(color, size, x+(size*2), y-(size*2))
-                self.draw_square(color, size, x+(size*3), y-(size*2))
-            elif letter == ' ':
-                x += (size*1)
-            elif letter == 'f':
-                self.draw_square(color, size, x, y)
-                self.draw_square(color, size, x, y-(size*1))
-                self.draw_square(color, size, x, y-(size*2))
-                self.draw_square(color, size, x, y-(size*3))
-                self.draw_square(color, size, x, y-(size*4))
-                self.draw_square(color, size, x, y+(size*1))
-                self.draw_square(color, size, x, y+(size*2))
-                self.draw_square(color, size, x, y+(size*3))
-                self.draw_square(color, size, x+(size*1), y+(size*4))
-                self.draw_square(color, size, x+(size*2), y+(size*4))
-                self.draw_square(color, size, x-(size*1), y+(size*1))
-                self.draw_square(color, size, x+(size*1), y+(size*1))
-                self.draw_square(color, size, x+(size*2), y+(size*1))
-            elif letter == 'g':
-                self.draw_square(color, size, x, y)
-                self.draw_square(color, size, x-(size*1), y)
-                self.draw_square(color, size, x+(size*1), y)
-                self.draw_square(color, size, x+(size*2), y)
-                self.draw_square(color, size, x+(size*3), y)
-                self.draw_square(color, size, x+(size*3), y+(size*1))
-                self.draw_square(color, size, x+(size*3), y+(size*2))
-                self.draw_square(color, size, x+(size*3), y+(size*3))
-                self.draw_square(color, size, x+(size*3), y+(size*4))
-                self.draw_square(color, size, x+(size*2), y+(size*4))
-                self.draw_square(color, size, x+(size*1), y+(size*4))
-                self.draw_square(color, size, x, y+(size*4))
-                self.draw_square(color, size, x-(size*1), y+(size*4))
-                self.draw_square(color, size, x-(size*2), y+(size*3))
-                self.draw_square(color, size, x-(size*2), y+(size*2))
-                self.draw_square(color, size, x-(size*2), y+(size*1))
-                self.draw_square(color, size, x+(size*3), y-(size*1))
-                self.draw_square(color, size, x+(size*2), y-(size*2))
-                self.draw_square(color, size, x+(size*1), y-(size*2))
-                self.draw_square(color, size, x, y-(size*2))
-                self.draw_square(color, size, x-(size*1), y-(size*2))
-                self.draw_square(color, size, x-(size*2), y-(size*2))
-            elif letter == 'h':
-                self.draw_square(color, size, x, y+(size*2))
-                self.draw_square(color, size, x+(size*1), y+(size*2))
-                self.draw_square(color, size, x+(size*2), y+(size*1))
-                self.draw_square(color, size, x+(size*2), y)
-                self.draw_square(color, size, x+(size*2), y-(size*1))
-                self.draw_square(color, size, x+(size*2), y-(size*2))
-                self.draw_square(color, size, x-(size*3), y-(size*2))
-                self.draw_square(color, size, x-(size*3), y-(size*1))
-                self.draw_square(color, size, x-(size*3), y)
-                self.draw_square(color, size, x-(size*3), y+(size*1))
-                self.draw_square(color, size, x-(size*2), y+(size*1))
-                self.draw_square(color, size, x-(size*1), y+(size*1))
-                self.draw_square(color, size, x-(size*3), y+(size*2))
-                self.draw_square(color, size, x-(size*3), y+(size*3))
-                self.draw_square(color, size, x-(size*3), y+(size*4))
-                self.draw_square(color, size, x-(size*3), y+(size*5))
-                self.draw_square(color, size, x-(size*3), y+(size*6))
-                self.draw_square(color, size, x-(size*3), y+(size*7))
-            elif letter == 'i':
-                self.draw_square(color, size, x, y)
-                self.draw_square(color, size, x, y+(size*1))
-                self.draw_square(color, size, x, y+(size*2))
-                self.draw_square(color, size, x, y+(size*3))
-                self.draw_square(color, size, x, y+(size*5))
-                self.draw_square(color, size, x, y-(size*1))
-                self.draw_square(color, size, x, y-(size*2))
-            elif letter == 'j':
-                self.draw_square(color, size, x, y)
-                self.draw_square(color, size, x, y+(size*1))
-                self.draw_square(color, size, x, y+(size*2))
-                self.draw_square(color, size, x, y+(size*3))
-                self.draw_square(color, size, x, y+(size*5))
-                self.draw_square(color, size, x, y-(size*1))
-                self.draw_square(color, size, x, y-(size*2))
-                self.draw_square(color, size, x-(size*1), y-(size*3))
-                self.draw_square(color, size, x-(size*2), y-(size*3))
-                self.draw_square(color, size, x-(size*3), y-(size*2))
-    def draw_square(self, color, size, x, y):
-        self.pen.penup()
-        self.pen.goto(x, y)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.fillcolor(color)
-        self.pen.pencolor(color)
-        for _ in range(4):
-            self.pen.forward(size)
-            self.pen.left(90)
-        self.pen.end_fill()
-        turtle.update()
-    def destroy_text(self):
-        self.pen.clear()
-    def show_typing_thingy(self):
-        pass
-        
+    const squareSize = 20;  // Size of each square in the drawing
+    const color = 'black';  // Color for the squares
 
-#remember to remove this part later, its just a test!
-klockcrafttext = KlockcraftText()
-klockcrafttext.draw_text('Just a test for the text.', 'black', 2, 0, 0)
+    function drawSquare(x, y, size) {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, size, size);
+    }
 
-turtle.mainloop()
+    function drawText(message, x, y) {
+        let offsetX = x;
+
+        for (let i = 0; i < message.length; i++) {
+            const letter = message[i];
+            switch (letter) {
+                case 'a':
+                    drawA(offsetX, y);
+                    break;
+                case 'b':
+                    drawB(offsetX, y);
+                    break;
+                case 'c':
+                    drawC(offsetX, y);
+                    break;
+                case 'd':
+                    drawD(offsetX, y);
+                    break;
+                case 'e':
+                    drawE(offsetX, y);
+                    break;
+                case 'f':
+                    drawF(offsetX, y);
+                    break;
+                case 'g':
+                    drawG(offsetX, y);
+                    break;
+                case 'i':
+                    drawI(offsetX, y);
+                    break;
+                case 'j':
+                    drawJ(offsetX, y);
+                    break;
+                case ' ':
+                    offsetX += squareSize;  // Space is a simple offset
+                    break;
+            }
+            offsetX += squareSize * 2;  // Add a gap between letters
+        }
+    }
+
+    function drawA(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x + squareSize, y, squareSize);
+        drawSquare(x + squareSize * 2, y, squareSize);
+        drawSquare(x + squareSize * 3, y - squareSize, squareSize);
+        drawSquare(x + squareSize * 3, y - squareSize * 2, squareSize);
+        drawSquare(x + squareSize * 2, y - squareSize * 2, squareSize);
+        drawSquare(x + squareSize, y - squareSize * 2, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+    }
+
+    function drawB(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+        drawSquare(x, y - squareSize * 2, squareSize);
+        drawSquare(x, y - squareSize * 3, squareSize);
+        drawSquare(x, y - squareSize * 4, squareSize);
+        drawSquare(x + squareSize, y, squareSize);
+        drawSquare(x + squareSize, y - squareSize, squareSize);
+        drawSquare(x + squareSize, y - squareSize * 3, squareSize);
+        drawSquare(x + squareSize * 2, y - squareSize * 2, squareSize);
+        drawSquare(x + squareSize * 2, y - squareSize * 3, squareSize);
+        drawSquare(x + squareSize * 3, y - squareSize * 3, squareSize);
+        drawSquare(x + squareSize * 3, y - squareSize * 4, squareSize);
+    }
+
+    function drawC(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+        drawSquare(x, y - squareSize * 2, squareSize);
+        drawSquare(x, y - squareSize * 3, squareSize);
+        drawSquare(x, y - squareSize * 4, squareSize);
+        drawSquare(x + squareSize, y - squareSize * 4, squareSize);
+        drawSquare(x + squareSize * 2, y - squareSize * 4, squareSize);
+    }
+
+    function drawD(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+        drawSquare(x, y - squareSize * 2, squareSize);
+        drawSquare(x, y - squareSize * 3, squareSize);
+        drawSquare(x, y - squareSize * 4, squareSize);
+        drawSquare(x + squareSize, y - squareSize * 4, squareSize);
+        drawSquare(x + squareSize * 2, y - squareSize * 3, squareSize);
+        drawSquare(x + squareSize * 3, y - squareSize * 2, squareSize);
+        drawSquare(x + squareSize * 3, y - squareSize, squareSize);
+        drawSquare(x + squareSize * 2, y, squareSize);
+    }
+
+    function drawE(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x + squareSize, y, squareSize);
+        drawSquare(x + squareSize * 2, y, squareSize);
+        drawSquare(x + squareSize * 3, y, squareSize);
+        drawSquare(x + squareSize * 3, y - squareSize, squareSize);
+        drawSquare(x + squareSize * 2, y - squareSize * 2, squareSize);
+        drawSquare(x + squareSize, y - squareSize * 2, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+        drawSquare(x - squareSize, y, squareSize);
+        drawSquare(x - squareSize, y - squareSize, squareSize);
+        drawSquare(x - squareSize, y - squareSize * 2, squareSize);
+    }
+
+    function drawF(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+        drawSquare(x, y - squareSize * 2, squareSize);
+        drawSquare(x, y - squareSize * 3, squareSize);
+        drawSquare(x, y - squareSize * 4, squareSize);
+        drawSquare(x + squareSize, y, squareSize);
+        drawSquare(x + squareSize * 2, y, squareSize);
+    }
+
+    function drawG(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x - squareSize, y, squareSize);
+        drawSquare(x + squareSize, y, squareSize);
+        drawSquare(x + squareSize * 2, y, squareSize);
+        drawSquare(x + squareSize * 3, y, squareSize);
+        drawSquare(x + squareSize * 3, y + squareSize, squareSize);
+        drawSquare(x + squareSize * 3, y + squareSize * 2, squareSize);
+        drawSquare(x + squareSize * 3, y + squareSize * 3, squareSize);
+        drawSquare(x + squareSize * 3, y + squareSize * 4, squareSize);
+    }
+
+    function drawI(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x, y + squareSize, squareSize);
+        drawSquare(x, y + squareSize * 2, squareSize);
+        drawSquare(x, y + squareSize * 3, squareSize);
+        drawSquare(x, y + squareSize * 5, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+        drawSquare(x, y - squareSize * 2, squareSize);
+    }
+
+    function drawJ(x, y) {
+        drawSquare(x, y, squareSize);
+        drawSquare(x, y + squareSize, squareSize);
+        drawSquare(x, y + squareSize * 2, squareSize);
+        drawSquare(x, y + squareSize * 3, squareSize);
+        drawSquare(x, y + squareSize * 5, squareSize);
+        drawSquare(x, y - squareSize, squareSize);
+        drawSquare(x, y - squareSize * 2, squareSize);
+        drawSquare(x - squareSize, y - squareSize * 3, squareSize);
+    }
+
+    // Example usage:
+    drawText('abcde', 50, 100);
